bf0e683e8b213413dded5cab29205145f9168e30
Solomon Hawk <solomon.hawk@viget.com>
2018-01-11 15:35:37 -0500

    Fix up a few things

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = () => {
-  return 'Its working!!'
+let generateAnimal = require('animals')
+let Moniker = require('moniker')
+let adjectives = Moniker.generator([Moniker.adjective])
+
+module.exports = () => `${uc(adjectives.choose())} ${uc(generateAnimal())}`
+
+function uc(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1)
 }
